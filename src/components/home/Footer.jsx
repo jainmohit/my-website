b70a50f529740a708aa3d0a2bc2e243909a177f1
Mohit Jain <jain.moheet@gmail.com>
2022-10-08 00:26:54 +1100

    [Mohit] Added changes

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -29,8 +29,8 @@ import {
           <div className="d-inline align-self-center">
           <h2 className="display-4 pb-3 text-center"><b>Get In Touch</b></h2>
           <p className="lead text-center" style={{fontSize:'22px'}}>If you have any questions, or just want to say hi, please feel free to email me at
-            <a href="mailto:anuragvaidhya786@gmail.com">{" "}
-              anuragvaidhya786@gmail.com
+            <a href="mailto:jain.moheet@gmail.com">{" "}
+              jain.moheet@gmail.com
             </a>{"."}
           </p>
           
@@ -56,10 +56,10 @@ import {
       <a
         className="badge badge-dark"
         rel="noopener"
-        href="https://github.com/anuu1989"
+        href="https://github.com/jainmohit"
         aria-label="My GitHub"
       >
-        Anurag Vaidhya
+        Mohit Jain
       </a>{" "}
       using <i className="fab fa-react"></i>
     </footer>
